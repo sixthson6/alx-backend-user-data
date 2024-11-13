7c358e7c9a7580cbaa587d8c9ff2d4307b0b0971
sixthson6 <sudais2408@gmail.com>
2024-11-13 06:07:11 -0800

    Add forbidden

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -32,7 +32,7 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """ Forbidden handler
     """
-    return jsonify({"error": "forbidden"})
+    return jsonify({"error": "forbidden"}), 403
 
 
 if __name__ == "__main__":
